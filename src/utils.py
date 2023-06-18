0f4bf462efc8aaa56be7064fd5abff48c1422624
Gennadiy <renardes@gmail.com>
2023-06-18 12:55:47 +0500

    добавление read_players()

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,21 @@
 """
 Вспомогательные функции.
 """
+
+# стандартная библиотека
+from configparser import ConfigParser
+from pathlib import Path
+
+
+def read_players(players_path: Path | str) -> dict[str, dict[str, int]]:
+    config = ConfigParser()
+    config.read(players_path)
+    config = {
+        player_name: {
+            key: int(value)
+            for key, value in config[player_name].items()
+        }
+        for player_name in config.sections()
+    }
+    return config
+
