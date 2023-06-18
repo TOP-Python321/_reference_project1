f11164b8ddbae6dfcb50c03111e0bd6995e6408f
Gennadiy <renardes@gmail.com>
2023-06-18 12:42:18 +0500

    структура суперцикла

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@
 Точка входа: управляющий код.
 """
 
+# проект
+import data
+
 # 1. Чтение файлов данных
 
 # 2. ЕСЛИ первый запуск:
@@ -12,5 +15,18 @@
 # суперцикл
 while True:
     # 4. Ожидание ввода команды
-    ...
+    command = input(data.PROMPT)
+
+    if command in data.COMMANDS['начать новую партию']:
+        ...
+
+    elif command in data.COMMANDS['загрузить существующую партию']:
+        ...
+
+    # elif ...
+
+    elif command in data.COMMANDS['выйти']:
+        break
+
+# 16. Обработка завершения работы приложения
 
