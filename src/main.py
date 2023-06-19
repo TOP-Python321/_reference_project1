5b8b6e5b213707c4b30d60056e580e67db670068
Gennadiy <renardes@gmail.com>
2023-06-19 10:15:10 +0500

    использование game()

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@
 
 # проект
 import data
+import game
 import player
 import utils
 
@@ -23,15 +24,29 @@ while True:
     command = input(data.PROMPT)
 
     if command in data.COMMANDS['начать новую партию']:
+        # 5. Запрос режима игры:
         ...
+        # партия
+        result = game.game()
+        # 14. Обновление статистики в базе игроков и обновление файлов данных
+        if result is not None:
+            player.update_stats(result)
 
     elif command in data.COMMANDS['загрузить существующую партию']:
-        ...
+        # game.load()
+        result = game.game()
+        # 14. Обновление статистики в базе игроков и обновление файлов данных
+        if result is not None:
+            player.update_stats(result)
+            ...
 
     # elif ...
 
     elif command in data.COMMANDS['выйти']:
         break
+
+    # 15. Переход к этапу 4
+
 
 # 16. Обработка завершения работы приложения
 
