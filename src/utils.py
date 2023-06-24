b80a166395459d64fdcffd817406afd6415dffb1
Gennadiy <renardes@gmail.com>
2023-06-24 12:41:27 +0500

    минорная правка read_players()

--- src/utils.py
+++ src/utils.py
@@ -12,14 +12,13 @@ def read_players() -> bool:
     """Читает файл данных игроков, сохраняет информацию в соответствующую глобальную структуру данных. Возвращает True, если в файле данных игроков есть хотя бы одна запись, иначе False."""
     config = ConfigParser()
     config.read(data.PLAYERS_PATH)
-    config = {
+    data.players_db = {
         player_name: {
             key: int(value)
             for key, value in config[player_name].items()
         }
         for player_name in config.sections()
     }
-    data.players_db = config
     return bool(config)
 
 
