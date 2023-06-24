d79fa698c95cde90f39307ded2bbae94ad6b5e0b
Gennadiy <renardes@gmail.com>
2023-06-24 12:39:26 +0500

    добавление change_dim()

--- src/utils.py
+++ src/utils.py
@@ -45,3 +45,11 @@ def write_players() -> None:
 
 def write_saves() -> None:
     """"""
+
+
+def change_dim(new_dim: int) -> None:
+    """"""
+    data.dim = new_dim
+    data.dim_range = range(new_dim)
+    data.all_cells = new_dim**2
+
