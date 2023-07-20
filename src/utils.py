a8d721950b875e949710abd07b81ec289e1509ab
Gennadiy <renardes@gmail.com>
2023-07-20 17:23:50 +0500

    изменение change_dim()

--- src/utils.py
+++ src/utils.py
@@ -69,6 +69,7 @@ def change_dim(new_dim: int) -> None:
     data.dim_range = range(new_dim)
     data.all_cells = new_dim**2
     data.board = dict.fromkeys(range(1, data.all_cells+1), ' ')
+    data.MESSAGES['ход не в диапазоне'] = f' ! номер ячейки должен находиться в диапазоне от 0 до {data.all_cells-1} включительно'
 
 
 def field_template(data_width: int = None) -> str:
