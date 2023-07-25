b4948e8dad16ec36c903995fc2ad66bb0635e95a
Gennadiy <renardes@gmail.com>
2023-07-25 12:36:26 +0500

    использование render_all() и render_commands()

--- src/main.py
+++ src/main.py
@@ -5,19 +5,21 @@
 # проект
 import data
 import game
+import help
 import player
 import utils
 
+
+utils.change_dim(3)
 
 print(utils.header_text(data.APP_TITLE, level=1))
 
 # 1. Чтение файлов данных
 # 2. ЕСЛИ первый запуск:
 if utils.read_players():
-    # вывод титров
-    ...
+    # вывод раздела помощи
+    print(help.render_all())
 utils.read_saves()
-utils.change_dim(3)
 
 # 3. Запрос имени игрока
 player.get_player_name()
@@ -55,10 +57,16 @@ while True:
     elif command in data.COMMANDS['создать или переключиться на игрока']:
         player.get_player_name()
 
+    elif command in data.COMMANDS['отобразить раздел помощи']:
+        print(help.render_all())
+
     # elif command in data.COMMANDS['']:
 
     elif command in data.COMMANDS['выйти']:
         break
+
+    else:
+        print(help.render_commands())
 
     # 15. Переход к этапу 4
 
